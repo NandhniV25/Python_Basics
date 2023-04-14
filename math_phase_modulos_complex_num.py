29de7e9f7d78424630f64eda3849105df66cf751
venkat <venkatpselvam1@gmail.com>
2023-04-14 22:57:07 +0530

    math

--- math_phase_modulos_complex_num.py
+++ math_phase_modulos_complex_num.py
@@ -1,17 +1,5 @@
-"""
-The first line should contain the value of r.
-The second line should contain the value of o.
-A polar coordinate (r,o)
-
-is completely determined by modulus r and phase angle o.
-
-If we convert complex number z  to its polar coordinate, we find:
-r: Distance from z  to origin, i.e., square root of x^2+y^2
-o: Counter clockwise angle measured from the positive x-axis to the line segment that joins  to the origin.
-
-Python's cmath module provides access to the mathematical functions for complex numbers.
-"""
-import cmath
-z=complex(input())
-print(abs(z))#r
-print(cmath.phase(z))
+a=int(input())
+b=int(input())
+print(a//b)
+print(a%b)
+print(divmod(a,b))
---
+++ math_power.py
@@ -0,0 +1,14 @@
+import math
+a=float(input())
+b=float(input())
+m=float(input())
+print(int(math.pow(a,b)))
+print(int(math.pow(a,b)%m))
+
+import math
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+#print(int((math.pow(a,b))+(math.pow(c,d))))
+print(int(a**b+c**d))
