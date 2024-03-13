6a8fd6382a586b97038320aaa6b56993a1d69564
Nandhni G A <nandhni.25@gmail.com>
2024-03-13 15:09:14 +0530

    importing openAI python library and creating sample program

---
+++ OpenAI/chatGpt.py
@@ -0,0 +1,41 @@
+import openai
+import os
+
+openai.api_key = "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"
+print(openai.api_key)
+
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0,  # this is the degree of randomness of the model's output
+    )
+    print(response.choices[0].message["content"])
+    return response.choices[0].message["content"]
+
+
+prod_review = f"""
+Got this panda plush toy for my daughter's birthday, \
+who loves it and takes it everywhere. It's soft and \ 
+super cute, and its face has a friendly look. It's \ 
+a bit small for what I paid though. I think there \ 
+might be other options that are bigger for the \ 
+same price. It arrived a day earlier than expected, \ 
+so I got to play with it myself before I gave it \ 
+to her.
+"""
+
+prompt = f"""
+Your task is to generate a short summary of a product \
+review from an ecommerce site. 
+
+Summarize the review below, delimited by triple 
+backticks, in at most 30 words. 
+
+Review: ```{prod_review}```
+"""
+
+response = get_completion(prompt)
+print(response)
